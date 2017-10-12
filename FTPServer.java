42e3d2278531740d2cb5863ff3e66aea58fd8b88
Jonathan Stone <stonejo@mail.gvsu.edu>
2017-10-12 00:09:27 -0400

    Fixed problems in FTPServer.java

--- FTPServer.java
+++ FTPServer.java
@@ -3,27 +3,29 @@ import java.net.*;
 import java.util.*;
 
 public final class FTPServer {
+    private static ServerSocket welcomeSocket;
     public static void main(String argv[]) throws Exception {
 
 	String fromClient;
 	String clientCommand;
 	byte[] data;
-	
-        try {
-	ServerSocket welcomeSocket = new ServerSocket(5568);
-	} catch (IOException ioEx) {
-	    System.out.println("Unable to set up port.");
+	try {
+	    welcomeSocket = new ServerSocket(5568);
+	} catch(IOException ioEx) {
+	    System.out.println("Unable to set up port!");
 	    System.exit(1);
 	}
 	String frstln;
         
 	while(true)
             {
+		System.out.println("Waiting for a connection...");
                 Socket connectionSocket = welcomeSocket.accept();
                 
                 DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 		
                 BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+		System.out.println("Client accepted.");
 		
 		fromClient = inFromClient.readLine();
                 
@@ -37,31 +39,24 @@ public final class FTPServer {
 		if(clientCommand.equals("list:"))
 		    { 
 			Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
+			System.out.println("Data Socket opened.");
 			DataOutputStream  dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
 			File folder = new File(".");
-			File[] listOfFile = folder.listFiles();
+			File[] listOfFiles = folder.listFiles();
+			String temp;
 			for (int i=0; i < listOfFiles.length; i++) {
-			    if (listofFiles[i].isFile()) {
+			    if (listOfFiles[i].isFile()) {
 				//System.out.println("File " + listofFiles[i].getName());
-				data.parseByte(listOfFiles[i].getName());
+				//data.parseByte(listOfFiles[i].getName());
+				temp = listOfFiles[i].getName();
+				data = temp.getBytes();
 				dataOutToClient.write(data, 0, data.length);
-			    }
-			    
-			}		
+			    }    
+			}	
+			//dataOutToClient.write(data, 0, data.length);
+			dataSocket.close();
+			System.out.println("Data Socket closed.");
 		    }
-		
-		dataSocket.close();
-		System.out.println("Data Socket closed");
 	    }
-        
-	// ......................
-            
-	    
-	//if(clientCommand.equals("retr:"))
-	//  {
-		// ..............................
-		// ..............................
-	//  }
-	
     }
 }
