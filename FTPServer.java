15ab8fadf5e4d118c94004d30f9fee519a1dc361
Mitchell John Couturier <couturmi@mail.gvsu.edu>
2017-10-12 22:11:56 -0400

    Server now has basic functionality and is multithreaded

--- FTPServer.java
+++ FTPServer.java
@@ -3,60 +3,108 @@ import java.net.*;
 import java.util.*;
 
 public final class FTPServer {
+
     private static ServerSocket welcomeSocket;
+
     public static void main(String argv[]) throws Exception {
 
-	String fromClient;
-	String clientCommand;
-	byte[] data;
-	try {
-	    welcomeSocket = new ServerSocket(5568);
-	} catch(IOException ioEx) {
-	    System.out.println("Unable to set up port!");
-	    System.exit(1);
+		try {
+			welcomeSocket = new ServerSocket(5568);
+		} catch (IOException ioEx) {
+			System.out.println("Unable to set up port!");
+			System.exit(1);
+		}
+
+		while (true) {
+			Socket connectionSocket = welcomeSocket.accept();
+			System.out.println("Connected to " + connectionSocket.getRemoteSocketAddress().toString());
+
+			// Create ClientHandler thread to handle client
+			ClientHandler handler = new ClientHandler(connectionSocket);
+			handler.start();
+		}
 	}
-	String frstln;
-        
-	while(true)
-            {
-		System.out.println("Waiting for a connection...");
-                Socket connectionSocket = welcomeSocket.accept();
-                
-                DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-		
-                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-		System.out.println("Client accepted.");
-		
-		fromClient = inFromClient.readLine();
-                
-		StringTokenizer tokens = new StringTokenizer(fromClient);
-		
-		frstln = tokens.nextToken();
-		int port = Integer.parseInt(frstln);
-		System.out.println(port);
-		clientCommand = tokens.nextToken();
-                
-		if(clientCommand.equals("list:"))
-		    { 
-			Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
-			System.out.println("Data Socket opened.");
-			DataOutputStream  dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
-			File folder = new File(".");
-			File[] listOfFiles = folder.listFiles();
-			String temp;
-			for (int i=0; i < listOfFiles.length; i++) {
-			    if (listOfFiles[i].isFile()) {
-				//System.out.println("File " + listofFiles[i].getName());
-				//data.parseByte(listOfFiles[i].getName());
-				temp = listOfFiles[i].getName();
-				data = temp.getBytes();
-				dataOutToClient.write(data, 0, data.length);
-			    }    
-			}	
-			//dataOutToClient.write(data, 0, data.length);
-			dataSocket.close();
-			System.out.println("Data Socket closed.");
-		    }
-	    }
-    }
+}
+
+class ClientHandler extends Thread {
+
+	private Socket clientSocket;
+	private Scanner input;
+
+	public ClientHandler(Socket socket) {
+		//Set up reference to associated socket
+		clientSocket = socket;
+
+		try
+		{
+			input = new Scanner(clientSocket.getInputStream());
+		}
+		catch(IOException ioEx)
+		{
+			ioEx.printStackTrace();
+		}
+	}
+
+	public void run() {
+		String fromClient;
+		String clientCommand;
+		byte[] data;
+		String frstln;
+
+		do {
+			// read in initial command line from client
+			fromClient = input.nextLine();
+
+			StringTokenizer tokens = new StringTokenizer(fromClient);
+
+			frstln = tokens.nextToken();
+			int port = Integer.parseInt(frstln);
+			clientCommand = tokens.nextToken();
+
+			//if the command is "close", end this thread
+			if(clientCommand.equals("close")){
+				endConnection();
+				return;
+			}
+
+//			try {
+//				if (clientCommand.equals("list:")) {
+//					Socket dataSocket = new Socket(clientSocket.getInetAddress(), port);
+//					System.out.println("Data Socket opened.");
+//					DataOutputStream dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
+//					File folder = new File(".");
+//					File[] listOfFiles = folder.listFiles();
+//					String temp;
+//					for (int i = 0; i < listOfFiles.length; i++) {
+//						if (listOfFiles[i].isFile()) {
+//							//System.out.println("File " + listofFiles[i].getName());
+//							//data.parseByte(listOfFiles[i].getName());
+//							temp = listOfFiles[i].getName();
+//							data = temp.getBytes();
+//							dataOutToClient.write(data, 0, data.length);
+//						}
+//					}
+//					//dataOutToClient.write(data, 0, data.length);
+//					dataSocket.close();
+//					System.out.println("Data Socket closed.");
+//				}
+//			} catch (IOException ex) {
+//				ex.printStackTrace();
+//			}
+		} while (true);
+	}
+
+	/**
+	 * Closes the thread
+	 */
+	private void endConnection() {
+		System.out.println("Disconnecting from client "+clientSocket.getRemoteSocketAddress().toString());
+		input.close();
+		try {
+			clientSocket.close();
+		} catch(IOException ioEx) {
+			System.out.println("Unable to disconnect!");
+		}
+		System.out.println("Disconnected from client");
+	}
 }
