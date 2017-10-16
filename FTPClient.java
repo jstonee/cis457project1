2386ec15613261ecf7451a00d9a76ab6a75010b2
gabevansolkema <gabejvansolkema@gmail.com>
2017-10-16 09:01:41 -0400

    Added retr functionality.

--- FTPClient.java
+++ FTPClient.java
@@ -48,49 +48,69 @@ public class FTPClient {
                         System.out.println(dataFromServer);
                         dataFromServer = inData.readLine();
                     }
-		    inData.close();
+                    inData.close();
                     dataSocket.close();
-		    welcomeData.close();
+                    welcomeData.close();
+
                 } else if (input.startsWith("retr")) {
                     tokens = new StringTokenizer(input);
                     String file = tokens.nextToken();
                     file = tokens.nextToken();
                     System.out.println("Requesting " + file + " from server");
+
+                    int port1 = port + 2;
+                    ServerSocket welcomeData = new ServerSocket(port1);
+                    toServer.writeBytes(port1 + " " + input + " " + '\n');
+                    Socket dataSocket = welcomeData.accept();
+                    System.out.println("Data socket open, retrieving file now");
+
+                    BufferedInputStream inData = new BufferedInputStream(new DataInputStream(dataSocket.getInputStream()));
+                    FileOutputStream newFile = new FileOutputStream(new File(file));
+                    byte[] buffer = new byte[8192];
+				    int count;
+				    while((count = inData.read(buffer)) > 0) {
+                        newFile.write(buffer, 0, count);
+				    }
+
+				    newFile.close();
+				    inData.close();
+                    dataSocket.close();
+                    welcomeData.close();
+
                 } else if (input.startsWith("stor")) {
                     tokens = new StringTokenizer(input);
                     String file = tokens.nextToken();
                     file = tokens.nextToken();
-		    File openFile = new File(file);
-		    if(openFile.exists()) {
-			System.out.println("Storing " + file + " to server");
-			byte[] buffer = new byte[8192];
-			int port1 = port + 2;
-			ServerSocket welcomeData = new ServerSocket(port1);
-			toServer.writeBytes(port1 + " " + input + " " + '\n');
-			
-			Socket dataSocket = welcomeData.accept();
-			System.out.println("Data socket open, sending file now");
-			
-			DataOutputStream sendFile = new DataOutputStream(dataSocket.getOutputStream());
-			BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
-			int count;
-			while((count = in.read(buffer)) > 0) {
-			    sendFile.write(buffer, 0, count);
-			}
-			in.close();
-			sendFile.close();
-			dataSocket.close();
-			welcomeData.close();
-		    }
-		    else {
-			System.out.println("File does not exist.");
-		    }
+		            File openFile = new File(file);
+		            if(openFile.exists()) {
+                        System.out.println("Storing " + file + " to server");
+                        byte[] buffer = new byte[8192];
+                        int port1 = port + 2;
+                        ServerSocket welcomeData = new ServerSocket(port1);
+                        toServer.writeBytes(port1 + " " + input + " " + '\n');
+                        Socket dataSocket = welcomeData.accept();
+                        System.out.println("Data socket open, sending file now");
+                        DataOutputStream sendFile = new DataOutputStream(dataSocket.getOutputStream());
+                        BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+                        int count;
+                        while((count = in.read(buffer)) > 0) {
+                            sendFile.write(buffer, 0, count);
+                        }
+                        in.close();
+                        sendFile.close();
+                        dataSocket.close();
+                        welcomeData.close();
+                    } else {
+                        System.out.println("File does not exist.");
+                    }
+
                 } else if (input.equals("close")) {
                     System.out.println("Closing Control Socket");
                     toServer.writeBytes(0 + " " + input + " " + '\n');
                     ControlSocket.close();
                     System.out.println("Exiting");
                     System.exit(0);
+
                 } else {
                     System.out.println("Invalid command");
                 }
--- FTPServer.java
+++ FTPServer.java
@@ -1,4 +1,4 @@
-import java.io.*; 
+import java.io.*;
 import java.net.*;
 import java.util.*;
 
@@ -7,7 +7,6 @@ public final class FTPServer {
     private static ServerSocket welcomeSocket;
 
     public static void main(String argv[]) throws Exception {
-
 		try {
 			welcomeSocket = new ServerSocket(5568);
 		} catch (IOException ioEx) {
@@ -32,7 +31,7 @@ class ClientHandler extends Thread {
 	private Scanner input;
 
 	public ClientHandler(Socket socket) {
-		//Set up reference to associated socket
+        //Set up reference to associated socket
 		clientSocket = socket;
 
 		try
@@ -87,6 +86,25 @@ class ClientHandler extends Thread {
 					dataOutToClient.close();
 					dataSocket.close();
 				}
+                else if(clientCommand.equals("retr")) {
+                    clientCommand = tokens.nextToken();
+                    Socket dataSocket = new Socket(clientSocket.getInetAddress(), port);
+                    DataOutputStream dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
+
+                    File openFile = new File(clientCommand);
+                    if (openFile.exists()) {
+                        byte[] buffer = new byte[8192];
+                        BufferedInputStream in = new BufferedInputStream(new FileInputStream(clientCommand));
+                        int count;
+                        while((count = in.read(buffer)) > 0) {
+                            dataOutToClient.write(buffer, 0, count);
+                        }
+
+                        in.close();
+                        dataOutToClient.close();
+                        dataSocket.close();
+                    }
+                }
 				else if(clientCommand.equals("stor")) {
 				    clientCommand = tokens.nextToken();
 				    Socket dataSocket = new Socket(clientSocket.getInetAddress(), port);
@@ -95,7 +113,7 @@ class ClientHandler extends Thread {
 				    byte[] buffer = new byte[8192];
 				    int count;
 				    while((count = dataFromClient.read(buffer)) > 0) {
-					file.write(buffer, 0, count);
+                        file.write(buffer, 0, count);
 				    }
 				    file.close();
 				    dataFromClient.close();
